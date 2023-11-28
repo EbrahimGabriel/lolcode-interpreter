f64807054b2a0781855ae320827c257a3674b971
EbrahimGabriel <emgabriel@up.edu.ph>
2023-11-28 23:30:40 +0800

    added categorized regex

--- project.py
+++ project.py
@@ -23,54 +23,69 @@ class LOLCODE_Interpreter(tk.Tk):
         self.lexemes = []
 
         #regexes to consider
-        self.identifiers = [r'[a-zA-Z][a-zA-Z0-9_]*']
+        #categorized
+        self.identifiers = [r'^([a-zA-Z][a-zA-Z0-9_]*)$']
+        self.numbr = r'^((-?[1-9]+)|(0))$'
+        self.numbar = r'^((-?[1-9][0-9]*\.[0.9]+)|(-?[0\.[0.9]+))$'
+        self.yarn = r'^"[^"]*"$'
+        self.troof = r'^(WIN|FAIL)$'
+        self.datatype = r'^(NUMBA?R|YARN|TROOF)$'
+        self.progstart = r'^HAI$'
+        self.progend = r'^KTHXBYE$'
+        self.vardecstart = r'^WAZZUP$'
+        self.vardecend = r'^BUHBYE$'
+        self.vardec = r'^I HAS A$'
+        self.varinit = r'^ITZ$'
+        self.datatypes = r'^(NOOB|NUMBA?R|YARN|TROOF)$'
+        self.input = r'^GIMMEH$'
+        self.output = r'^VISIBLE$'
+        self.concatoperator = r'^\+$'
+        self.arithmetic = r'^(SUM OF|DIFF OF|PRODUKT OF|QUOSHUNT OF|MOD OF|BIGGR OF|SMALLR OF)$'
+        self.opsep = r'^AN$'
+        self.concat = r'^SMOOSH$'
+        self.boolean = r'^(BOTH OF|EITHER OF|WON OF|NOT|ALL OF|ANY OF)$'
+        self.endlist = r'^MKAY$'
+        self.comparison = r'^(BOTH SAEM|DIFFRINT)$'
+        self.typecast = r'^MAEK$'
+        self.recast = r'^IS NOW A$'
+        self.assign = r'^R$'
+        self.ifstart = r'^O RLY\?$'
+        self.ifif = r'^YA RLY$'
+        self.elseif = r'^MEBBE$'
+        self.elsekey = r'^NO WAI$'
+        self.flowend = r'^OIC$'
+        self.switchstart = r'^WTF\?'
+        self.switchcase = r'^OMG$'
+        self.switchdef = r'^OMGWTF$'
+        self.loopstart = r'^IM IN YR$'
+        self.loopend = r'^IM OUTTA YR$'
+        self.increment = r'^UPPIN$'
+        self.decrement = r'^NERFIN$'
+        self.loopcond = r'^(TIL|WILE)$'
+        self.breakkey = r'^GTFO$'
+        self.funcstart = r'^HOW IZ I$'
+        self.funcend = r'^IF U SAY SO$'
+        self.params = r'^YR$'
+        self.returnkey = r'^FOUND YR$'
+        self.funccall = r'^I IZ$'
+        self.linebreak = r'^\n$'
 
-        self.literals = [r'(-?[1-9]+)|(0)', r'(-?[1-9][0-9]*\.[0.9]+)|(-?[0\.[0.9]+)', r'"[^"]*"', r'WIN|FAIL', r'NUMBA?R|YARN|TROOF']
+        self.comments = r'(( BTW .*\n)|(OBTW .*\n)|(TLDR\n))'
 
+        #used in tokenizing
         self.spacedkeywords = [r'I HAS A ', r'SUM OF ', r'DIFF OF ', r'PRODUKT OF ', r'QUOSHUNT OF ', r'MOD OF '
         , r'BIGGR OF ', r'SMALLR OF ', r'BOTH OF ', r'EITHER OF ', r'WON OF ', r'ANY OF ', r'ALL OF ', r'BOTH SAEM '
         , r'IS NOW A ', r'O RLY\? ', r'YA RLY ', r'NO WAI ', r'IM IN YR ', r'IM OUTTA YR ', r'HOW IZ I ', r'IF U SAY SO '
         , r'FOUND YR ', r'I IZ ']
 
-        self.keywords = [r'HAI', r'KTHXBYE', r'WAZZUP', r'BUHBYE', r'ITZ', r'R', r'NOT', r'DIFFRINT', r'SMOOSH', r'MAEK', r'A'
-        , r'VISIBLE', r'GIMMEH', r'MEBBE', r'OIC', r'WTF\?', r'OMG', r'OMGWTF', r'UPPIN', r'NERFIN', r'YR', r'TIL', r'WILE'
-        , r'GTFO', r'MKAY']
+        self.spacedyarn = r'"[^"]*"'
 
-        self.comments = r'( BTW .*\n)|(OBTW .*\n)|(TLDR\n)'
-        
         '''
         notes:
+        + is the concat operator for VISIBLE
         NUMBAR, NUMBR, YARN regex literals were changed to remove leading zeroes from NUMBR/NUMBARS and disallow " chars inside the YARN 
-        
-        categories:
-        HAI/KTHXBYE = program start/end
-        WAZZUP/BUHBYE = var declaration area start/end
-        I HAS A/ITZ = var declaration/initialization
-        NOOB/NUMBR/NUMBAR/YARN/TROOF = datatypes
-        
-        AN = operand separator
-        VISIBLE/GIMMEH = I/O
-        + = concatenation operator for VISIBLE
-        SUM OF, DIFF OF, PRODUKT OF, QUOSHUNT OF, MOD OF, BIGGR OF, SMALLR OF = arithmetic/mathematical 
-        SMOOSH = concatenation
-        BOTH OF, EITHER OF, WON OF, NOT, ALL OF, ANY OF = boolean
-        MKAY = end of list for all of and any of?
-        BOTH SAEM, DIFFRINT = comparison
-        MAEK/IS NOW A = typecast/recast
-        R = assignment
-        O RLY?, YA RLY, MEBBE, NO WAI = if then
-        WTF?, OMG, OMGWTF = switch 
-        OIC = if then/switch end
-        IM IN YR, IM OUTTA YR = loop start/end
-        TIL/WILE = loop condition
-        UPPIN/NERFIN = increment/decrement
-        GTFO = break
-        HOW IZ I/IF U SAY SO = function start/end
-        YR = func parameters
-        FOUND YR = return
-        I IZ = function call
         '''
-    
+
     #open a lolcode file
     def select_input(self):
         input_filename = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select a File", filetypes=(("LOLCODE files", "*.lol"), ("all files", "*.*")))
@@ -80,8 +95,9 @@ class LOLCODE_Interpreter(tk.Tk):
     
     #tokenizes one line and returns its tokens
     def tokenize_line(self, line):
-        #remove comments and replace with newline
-        temp = re.sub(self.comments,  '\n', line)
+        #remove comments
+        temp = re.sub(self.comments,  '', line)
+        temp = re.sub('\n', '', temp)
 
         count = 0
         substrings = []
@@ -95,10 +111,10 @@ class LOLCODE_Interpreter(tk.Tk):
                 substrings.append(regexstring)
         
         #get YARNs with spaces between
-        yarn = re.findall(self.literals[2], temp)
+        yarn = re.findall(self.spacedyarn, temp)
         if yarn: 
             for match in yarn:
-                temp = re.sub(self.literals[2], "{"+str(count)+"}", temp, 1)
+                temp = re.sub(self.spacedyarn, "{"+str(count)+"}", temp, 1)
                 count = count+1
                 substrings.append(match)
         
@@ -110,14 +126,13 @@ class LOLCODE_Interpreter(tk.Tk):
             for j in range(len(tokens)):
                 if tokens[j] == tempstr:
                     tokens[j] = substrings[i]
-                if tokens[j] == tempstr2:
-                    tokens[j] = substrings[i]+"\n"
-                    
+        tokens.append('\n')
         print(tokens)
         return tokens
     
     #identifies a token
     def identify_token(self, token):
+        
         pass
 
 #testing
